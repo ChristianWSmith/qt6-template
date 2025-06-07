9d9990488299a3beb61095ef93caaa76ac81089b
Christian Smith <smith.christian.william@gmail.com>
2025-06-07 15:15:55 -0400

    CI

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/icons/app_icon.png"));
+    QGuiApplication::setDesktopFileName("com.example.MyApp");
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
