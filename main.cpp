dae4beefb67a28c08f1cc44bf2854645eeee82cd
Christian Smith <56562906+ChristianWSmith@users.noreply.github.com>
2025-06-08 19:16:48 -0400

    CI (#1)
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * Update ci.yml
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * CI

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/app_icon.png"));
+    QGuiApplication::setDesktopFileName("com.example.MyApp");
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "qt-template_" + QLocale(locale).name();
+        const QString baseName = "MyApp_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
