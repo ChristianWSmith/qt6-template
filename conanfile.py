f6523d8f478ba0edea6437ecea08a9c76024da10
Christian Smith <smith.christian.william@gmail.com>
2025-06-09 13:59:04 -0400

    conan?

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,12 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeDeps, CMakeToolchain, CMake
+from conan.tools.microsoft import VCVars
 import os
 
 class MyAppConan(ConanFile):
     name = os.environ['APP_NAME']
     version = os.environ['APP_VERSION']
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeDeps", "CMakeToolchain"
     requires = [
         "fmt/10.2.1",
         "vulkan-loader/1.3.268.0",
@@ -15,6 +15,17 @@ class MyAppConan(ConanFile):
 
     def layout(self):
         self.folders.build = "build"
+
+    def generate(self):
+        cmake = CMakeDeps(self)
+        cmake.generate()
+
+        if os.environ['PLATFORM'] == "windows":
+            ms = VCVars(self)
+            ms.generate()
+
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
