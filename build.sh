8e09408f24b95c67890f97208ef892b59aaf4fb4
Christian Smith <smith.christian.william@gmail.com>
2025-06-09 15:31:08 -0400

    conan?

--- build.sh
+++ build.sh
@@ -12,13 +12,20 @@ pipenv run conan install . \
   --profile:build="${CONAN_PROFILE}" \
   --profile:host="${CONAN_PROFILE}"
 
-echo "--- conanvcvars.bat ---"
-cat $(find "${BUILD_DIR}" -name conanvcvars.bat)
-echo "--- conanvcvars.bat ---"
-
 cmake -B "${BUILD_DIR}" \
   -DCMAKE_TOOLCHAIN_FILE="${CONAN_TOOLCHAIN}" \
   -DCMAKE_PREFIX_PATH="${QT_CMAKE_DIR}" \
   -DCMAKE_BUILD_TYPE=${CMAKE_CONFIG} \
   -DQT_DEBUG_FIND_PACKAGE=ON
-cmake --build "${BUILD_DIR}" --parallel --config ${CMAKE_CONFIG}
+
+if [[ "${PLATFORM}" == "windows" ]]; then
+  VCVARSALL='C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat'
+  CMD_LINE=$(cat <<EOF
+call "${VCVARSALL}" amd64 && ^
+cmake --build "${BUILD_DIR}" --parallel --config "${CMAKE_CONFIG}"
+EOF
+  )
+  cmd.exe /c "${CMD_LINE}"
+else
+  cmake --build "${BUILD_DIR}" --parallel --config ${CMAKE_CONFIG}
+fi
