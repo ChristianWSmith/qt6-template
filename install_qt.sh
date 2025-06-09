759cb0ad59cc38ce4949630f56c90e7f3bdb2a53
Christian Smith <smith.christian.william@gmail.com>
2025-06-08 20:57:45 -0400

    more CI stuff

--- install_qt.sh
+++ install_qt.sh
@@ -6,8 +6,8 @@ source ./env.sh
 # Optional: Add modules conditionally
 QT_MODULES="${QT_MODULES:-}"
 MODULE_ARGS=""
-if [[ -n "$QT_MODULES" ]]; then
-  MODULE_ARGS="-m $QT_MODULES"
+if [[ -n "{$QT_MODULES}" ]]; then
+  MODULE_ARGS="-m ${QT_MODULES}"
 fi
 
-pipenv run aqt install-qt "$AQT_PLATFORM" desktop "$QT_VERSION" "$COMPILER_NAME" -O "Qt" $MODULE_ARGS
+pipenv run aqt install-qt "${AQT_PLATFORM}" desktop "${QT_VERSION}" "${COMPILER_NAME}" -O "Qt" ${MODULE_ARGS}
