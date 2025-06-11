04832ed306b9d7c0ae79a298d19a615b9caa13ca
Christian Smith <smith.christian.william@gmail.com>
2025-06-10 20:50:30 -0400

    polish+wayland

--- scripts/install_qt.sh
+++ scripts/install_qt.sh
@@ -10,5 +10,5 @@ if [[ -n "{$QT_MODULES}" ]]; then
   MODULE_ARGS="-m ${QT_MODULES}"
 fi
 
-pipenv install --dev
+installPipenv
 pipenv run aqt install-qt "${AQT_PLATFORM}" desktop "${QT_VERSION}" "${COMPILER_NAME}" -O "${QT_INSTALL_DIR}" ${MODULE_ARGS}
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/env.sh
 
-export CMAKE_BUILD_TYPE="${1:-Release}"
+export CMAKE_BUILD_TYPE="${1:-Debug}"
 
 source ${SCRIPT_DIR}/build.sh "${CMAKE_BUILD_TYPE}"
 
