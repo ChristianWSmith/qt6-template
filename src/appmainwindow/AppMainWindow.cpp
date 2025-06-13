c3f3866880dfe6186d48bc410bff9e48eac13d87
Christian Smith <smith.christian.william@gmail.com>
2025-06-12 21:36:32 -0400

    remove comments

--- src/appmainwindow/AppMainWindow.cpp
+++ src/appmainwindow/AppMainWindow.cpp
@@ -1,20 +1,15 @@
-
-//---src/appmainwindow/AppMainWindow.cpp---
-
 #include "AppMainWindow.h"
 
-// Includes for Counter feature concrete classes
 #include "../features/counter/model/CounterModel.h"
 #include "../features/counter/presenter/CounterPresenter.h"
 #include "../features/counter/widget/CounterWidget.h"
 
-// Includes for AppLog feature concrete classes
 #include "../features/applog/model/AppLogModel.h"
 #include "../features/applog/presenter/AppLogPresenter.h"
 #include "../features/applog/widget/AppLogWidget.h"
 
-#include <QHBoxLayout> // Required for horizontal layout
-#include <QWidget>     // Required for container widget
+#include <QHBoxLayout>
+#include <QWidget>
 
 AppMainWindow::AppMainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::AppMainWindow) {
@@ -22,29 +17,22 @@ AppMainWindow::AppMainWindow(QWidget *parent)
 
   setWindowTitle(APP_NAME);
 
-  // Initialize Counter feature components
   m_counterModel = new CounterModel(this);
   m_counterWidgetView = new CounterWidget(this);
   m_counterPresenter =
       new CounterPresenter(m_counterModel, m_counterWidgetView, this);
 
-  // Initialize AppLog feature components
   m_appLogModel = new AppLogModel(this);
   m_appLogWidgetView = new AppLogWidget(this);
   m_appLogPresenter =
       new AppLogPresenter(m_appLogModel, m_appLogWidgetView, this);
 
-  // Create a container widget and set a horizontal layout
-  // The central widget of a QMainWindow can only have one child,
-  // so we use a container widget to hold multiple sub-widgets.
   QWidget *containerWidget = new QWidget(this);
   QHBoxLayout *mainLayout = new QHBoxLayout(containerWidget);
 
-  // Add CounterWidget and AppLogWidget to the horizontal layout
   mainLayout->addWidget(m_counterWidgetView);
   mainLayout->addWidget(m_appLogWidgetView);
 
-  // Set the container widget as the central widget of the main window
   setCentralWidget(containerWidget);
 }
 
--- src/appmainwindow/AppMainWindow.h
+++ src/appmainwindow/AppMainWindow.h
@@ -1,16 +1,14 @@
-//---src/appmainwindow/AppMainWindow.h---
+
 #ifndef APP_MAIN_WINDOW_H
 #define APP_MAIN_WINDOW_H
 
 #include "ui_AppMainWindow.h"
 #include <QMainWindow>
 
-// Forward declarations for Counter feature
 class ICounterModel;
 class ICounterWidget;
 class ICounterPresenter;
 
-// Forward declarations for AppLog feature
 class IAppLogModel;
 class IAppLogWidget;
 class IAppLogPresenter;
@@ -29,12 +27,10 @@ public:
 private:
   Ui::AppMainWindow *ui;
 
-  // Counter feature components
   ICounterModel *m_counterModel;
   ICounterWidget *m_counterWidgetView;
   ICounterPresenter *m_counterPresenter;
 
-  // AppLog feature components
   IAppLogModel *m_appLogModel;
   IAppLogWidget *m_appLogWidgetView;
   IAppLogPresenter *m_appLogPresenter;
