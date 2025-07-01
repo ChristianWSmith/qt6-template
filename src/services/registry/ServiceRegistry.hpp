946d8e0dbb9d3a8eeb18c9f53229dbd9c36a1122
Christian Smith <smith.christian.william@gmail.com>
2025-07-01 15:17:55 -0400

    cleanup

--- src/services/registry/ServiceRegistry.hpp
+++ src/services/registry/ServiceRegistry.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../events/system/EventSystem.hpp"
+#include <QCoreApplication>
 #include <QObject>
 #include <optional>
 
@@ -18,41 +19,34 @@ public:
 
   template <typename InputEvent, typename OutputEvent, typename Callable>
   static void registerOptionalService(Callable &&func);
-
-private:
-  static QObject *serviceOwner();
 };
-
-// Singleton QObject for lifetime management
-inline QObject *ServiceRegistry::serviceOwner() {
-  static QObject owner; // Lives until program shutdown
-  return &owner;
-}
 
 template <typename InputEvent, typename OutputEvent, typename Callable>
 void ServiceRegistry::registerService(Callable &&func) {
   events::subscribe<InputEvent>(
-      serviceOwner(),
+      QCoreApplication::instance(),
       [func = std::forward<Callable>(func)](const InputEvent &inputEvent) {
-        events::publish<OutputEvent>(func(inputEvent));
+        events::publish(func(inputEvent));
       });
 }
 
 template <typename InputEvent, typename Callable>
 void ServiceRegistry::registerOneWayService(Callable &&func) {
   events::subscribe<InputEvent>(
-      serviceOwner(), [func = std::forward<Callable>(func)](
-                          const InputEvent &inputEvent) { func(inputEvent); });
+      QCoreApplication::instance(),
+      [func = std::forward<Callable>(func)](const InputEvent &inputEvent) {
+        func(inputEvent);
+      });
 }
 
 template <typename InputEvent, typename OutputEvent, typename Callable>
 void ServiceRegistry::registerOptionalService(Callable &&func) {
   events::subscribe<InputEvent>(
-      serviceOwner(),
+      QCoreApplication::instance(),
       [func = std::forward<Callable>(func)](const InputEvent &inputEvent) {
         std::optional<OutputEvent> out = func(inputEvent);
         if (out) {
-          events::publish<OutputEvent>(*out);
+          events::publish(*out);
         }
       });
 }
