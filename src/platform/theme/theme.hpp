5fcb9653b1252a1e77be3f68433f5ea94458355c
Christian Smith <smith.christian.william@gmail.com>
2025-07-03 08:32:53 -0400

    theme system set up

--- src/platform/theme/theme.hpp
+++ src/platform/theme/theme.hpp
@@ -1,39 +1,56 @@
 #pragma once
 
-#ifdef Q_OS_WIN
 #include <QApplication>
+#include <QDebug>
 #include <QFile>
+#include <QOperatingSystemVersion>
 #include <QPalette>
 #include <QStyle>
 #include <QStyleFactory>
+
+#ifdef Q_OS_WIN
 #include <windows.h>
 
-bool isDarkModeEnabled() {
+bool isWindowsDarkModeEnabled() {
   DWORD value = 1;
   DWORD dataSize = sizeof(value);
-  RegGetValueW(
-      HKEY_CURRENT_USER,
-      L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
-      L"AppsUseLightTheme", RRF_RT_REG_DWORD, nullptr, &value, &dataSize);
+  if (RegGetValueW(
+          HKEY_CURRENT_USER,
+          L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+          L"AppsUseLightTheme", RRF_RT_REG_DWORD, nullptr, &value,
+          &dataSize) != ERROR_SUCCESS) {
+    return false; // Assume light mode if key can't be read
+  }
   return value == 0;
 }
+#else
+bool isWindowsDarkModeEnabled() { return false; };
 #endif
+
+QString loadQSS(const QString &path) {
+  QFile file(path);
+  if (!file.open(QFile::ReadOnly | QFile::Text)) {
+    qWarning("Could not open QSS file: %s", qUtf8Printable(path));
+    return "";
+  }
+  return QString::fromUtf8(file.readAll()).trimmed();
+}
 
 void setTheme() {
-#ifdef Q_OS_WIN
-  QApplication::setStyle(QStyleFactory::create("Fusion"));
-
   QString filePath;
-  if (isDarkModeEnabled()) {
-    filePath = ":/styles/windows_dark.qss";
+
+  if (QOperatingSystemVersion::currentType() ==
+      QOperatingSystemVersion::Windows) {
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+    filePath = isWindowsDarkModeEnabled() ? ":/styles/windows/dark.qss"
+                                          : ":/styles/windows/light.qss";
+  } else if (QOperatingSystemVersion::currentType() ==
+             QOperatingSystemVersion::MacOS) {
+    filePath = ":/styles/macos/base.qss";
   } else {
-    filePath = ":/styles/windows_light.qss";
+    filePath = ":/styles/linux/base.qss";
   }
 
-  QFile styleFile(filePath);
-  if (styleFile.open(QFile::ReadOnly)) {
-    QString styleSheet = QLatin1String(styleFile.readAll());
-    qApp->setStyleSheet(styleSheet);
-  }
-#endif
+  QString qss = loadQSS(filePath) + loadQSS(":/styles/custom.qss");
+  qApp->setStyleSheet(qss);
 }
